d8afe7640e95b273db7e9cc373908e348ad12359
Ayushx13 <ayushpofficialx@gmail.com>
2026-04-06 00:41:17 +0530

    feat(server): connect database before starting server

--- finance-backend/server.js
+++ finance-backend/server.js
@@ -1,9 +1,11 @@
 import "./src/config/config.js";
-import {app} from "./app.js";
+import connectDB from "./src/config/db.js";
+import { app } from "./app.js";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
+await connectDB();
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`🚀 Server is running on port ${PORT}`);
 });
